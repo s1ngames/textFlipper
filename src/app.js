f0f2ad0f5c8c797dc87b5d3235cd04099a7cb957
Nir Beck <nir@beckermus.com>
2018-05-20 08:48:43 +0300

    fixed some problems

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
